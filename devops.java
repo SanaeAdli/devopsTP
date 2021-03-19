db701a91a6ff13ee5855e886bcb9e25e9ffef7d3
SanaeAdli <65692170+SanaeAdli@users.noreply.github.com>
2021-03-19 12:07:16 +0100

    Update devops.java

--- devops.java
+++ devops.java
@@ -2,7 +2,7 @@ package devopsTP;
 
 public class devops {
 	 public static void main(String[] args) {
-	        System.out.println("Hello, World!We are Imane and Sanae"); 
+	        System.out.println("Hello, World!We are Imane and Sanae !!"); 
 	    }
 
 }
